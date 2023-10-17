cc4da2b59940aabf7fa484b90ca415d2fd7b7664
zbouzangad <khalildida93@gmail.com>
2023-10-17 15:08:13 +0100

    fetch using useEffect

--- src/App.js
+++ src/App.js
@@ -1,23 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useEffect, useState } from "react";
 
 function App() {
+  const [monsters, setMonsters] = useState([]);
+  useEffect(() => {
+    fetch("https://jsonplaceholder.typicode.com/users")
+      .then((resp) => resp.json())
+      .then((data) => {
+        setMonsters(data);
+      });
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {monsters.map((monster, i) => {
+        return (
+          <h1 key={i}>
+            {monster.name}, {i}
+          </h1>
+        );
+      })}
+      <h1></h1>
     </div>
   );
 }
